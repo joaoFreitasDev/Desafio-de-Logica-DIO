7ad75ba661745e0e609e486357e1a2dd8fb5ba5c
joaoFreitasDev <joao.vitor.ads86gmail.com>
2023-10-22 22:08:48 -0300

    commit adicionando laço de repetição no script

--- ClassificadorDeHerói.js
+++ ClassificadorDeHerói.js
@@ -1,33 +1,38 @@
 let heroi = {"nome":"", "xp": 0, "nivel":""}
 // Altere o nível e o nome do heroi:
 heroi["nome"] = "JOT!NH4"
-heroi["xp"] = 7500
 
+while(heroi["nivel"] != "RADIANTE"){
 
-if (heroi["XP"] <= 1000){
-    heroi["nivel"] = "FERRO"
+    heroi["xp"] += 1000
 
-}else if(heroi["xp"] >= 1001 && heroi["xp"] <= 2000){
-    heroi["nivel"] = "BRONZE"
+    if (heroi["XP"] <= 1000){
+        heroi["nivel"] = "FERRO"
 
-}else if(heroi["xp"] >= 2001 && heroi["xp"] <= 5000){
-    heroi["nivel"] = "PRATA"
+    }else if(heroi["xp"] >= 1001 && heroi["xp"] <= 2000){
+        heroi["nivel"] = "BRONZE"
 
-}else if(heroi["xp"] >= 5001 && heroi["xp"] <= 6000){
-    heroi["nivel"] = "OURO"
+    }else if(heroi["xp"] >= 2001 && heroi["xp"] <= 5000){
+        heroi["nivel"] = "PRATA"
 
-}else if(heroi["xp"] >= 6001 && heroi["xp"] <= 7000){
-    heroi["nivel"] = "PLATINA"
+    }else if(heroi["xp"] >= 5001 && heroi["xp"] <= 6000){
+        heroi["nivel"] = "OURO"
 
-}else if(heroi["xp"] >= 7001 && heroi["xp"] <= 8000){
-    heroi["nivel"] = "PLATINA"
+    }else if(heroi["xp"] >= 6001 && heroi["xp"] <= 7000){
+        heroi["nivel"] = "PLATINA"
 
-}else if(heroi["xp"] >= 8001 && heroi["xp"] <= 9000){
-    heroi["nivel"] = "IMORTAL"
+    }else if(heroi["xp"] >= 7001 && heroi["xp"] <= 8000){
+        heroi["nivel"] = "PLATINA"
 
-}else if(heroi["xp"] >= 9001){
-    heroi["nivel"] = "RADIANTE"
+    }else if(heroi["xp"] >= 8001 && heroi["xp"] <= 9000){
+        heroi["nivel"] = "IMORTAL"
 
+    }else if(heroi["xp"] >= 10001){
+        heroi["nivel"] = "RADIANTE"
+
+    }
+
+    console.log("O Herói de nome " + heroi['nome'] + " está no nível " + heroi["nivel"])
+    console.log("")
 }
 
-console.log("O Herói de nome " + heroi['nome'] + " está no nível " + heroi["nivel"])
